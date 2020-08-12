7c3000868fdf9761597d9a43777906c4c66d9670
E.T Anderson <anderson.eric.t@gmail.com>
2020-08-12 16:06:59 -0400

    Update product.py

--- product_cms/py/product.py
+++ product_cms/py/product.py
@@ -3,31 +3,29 @@ import os
 import generic_database as gdb
 import uuid
 
-def make_productdb():
-    productdb = gdb.GenericDatabase('')
-    return productdb
-
-
 class Product():
     def __init__(self, product_name):
         self.name = product_name
         self.id = uuid.uuid4()
 
     def save(self):
-        product_set = productdb.set('key', 'value')
-        if product_set:
-            product_saved = productdb.dumpdb()
-            if product_saved:
-                print(f'{self.name} successfully added!')
-                return
+        productdb = Product.get_db()
+        product_saved = productdb.set(self.id, self.name)
+        if product_saved:
+            print(f'{self.name} successfully added!')
+            return True
 
-    def delete(self, key):
-        product_deleted = productdb.delete(key)
+    def delete(self):
+        productdb = Product.get_db()
+        product_deleted = productdb.delete(self.id)
         if product_deleted:
-            print(f'{key} successfully removed from database!')
-            return
+            print(f'{self.name} successfully removed from database!')
+            self.id = None
+            return True
         else:
             return False
 
-
+    @staticmethod
+    def get_db():
+        return gdb.GenericDatabase(r'~\Documents\code\projects\product_cms\py\db.json')
 
